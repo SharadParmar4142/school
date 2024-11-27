903ef5b6859a3af409e4f09501f6d5b01a1ada72
SharadParmar4142 <145182110+SharadParmar4142@users.noreply.github.com>
2024-11-28 00:01:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const schoolRoutes = require('./routes/schoolRoutes');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
 connectDB();
